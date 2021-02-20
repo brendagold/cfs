21a1d32e61556bce48f5d325010a76fef0e453cf
Brenda <petersbrenda47@gmail.com>
2021-02-20 17:00:58 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+//import 'bulma/css/bulma.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Scores from './components/Scores';
+import Articles from './components/Articles';
+import Highlight from './components/Highlight';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Hero />
+      
+      <Articles />
+      <Highlight />
     </div>
   );
 }
