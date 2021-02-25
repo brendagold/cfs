d602b7e6d3a51814923fbf0b7aa5c238ae820ec8
Brenda <petersbrenda47@gmail.com>
2021-02-25 10:47:52 -0500

    m

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,33 +9,33 @@ const AboutUs = () => {
           <h2 className=" font-weight-bold d-inline">About us</h2>
         </div>
       </div>
-      <div class="row position-relative">
-        <div class="col-12 col-sm-8 col-md-6 col-lg-6">
-          <div class="card ml-md-2">
-            <img class="card-img" src={image1} alt="Bologna" />
-            <div class="card-img-overlay text-white d-flex flex-column justify-content-center"></div>
+      <div className="row position-relative">
+        <div className="col-12 col-sm-8 col-md-6 col-lg-6">
+          <div className="card ml-md-2">
+            <img className="card-img" src={image1} alt="Bologna" />
+            <div className="card-img-overlay text-white d-flex flex-column justify-content-center"></div>
           </div>
         </div>
-        <div class="col-12 col-sm-4 col-md-6 col-lg-5 contianer-about-card pl-0 shadow-lg">
-          <div class="overflow-y-hidden">
-            <span class="bg-primary px-3 py-2 overflow-y-hidden text-white font-size-lg">
+        <div className="col-12 col-sm-4 col-md-6 col-lg-5 contianer-about-card pl-0 shadow-lg">
+          <div className="overflow-y-hidden">
+            <span className="bg-primary px-3 py-2 overflow-y-hidden text-white font-size-lg">
               We Are
             </span>
           </div>
-          <div class="px-3 py-2">
-            <h2 class="overflow-y-hidden">Celberus Football Fantasy</h2>
-            <p class="font-italic color-gray">
+          <div className="px-3 py-2">
+            <h2 className="overflow-y-hidden">Celberus Football Fantasy</h2>
+            <p className="font-italic color-gray">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry
             </p>
-            <p class="card-text">
+            <p className="card-text">
               Lorem Ipsum has been the industry's standard dummy text ever since
               the 1500s, when an unknown printer took a galley of type and
               scrambled it to make a type specimen book.
             </p>
 
             <a href="">
-              <span class="bg-transparent pt-5 more-link text-dark">More</span>
+              <span className="bg-transparent pt-5 more-link text-dark">More</span>
             </a>
           </div>
         </div>
